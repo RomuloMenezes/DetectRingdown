e102be9dbfb7c7bdba63552ab6f66100409f970d
romulo_menezes <romulo.menezes@gmail.com>
2014-11-27 18:39:33 -0200

    Code change to allow easy parameterization of the time and frequency variation that define the normality range

--- Detect_Ringdown_delta_f.py
+++ Detect_Ringdown_delta_f.py
@@ -9,9 +9,10 @@ import datetime
 class DetectRingdown(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
 
-    delta_f_threshold = 0.05
-    delta_t_threshold = 20  # miliseconds. In order to be considered contiguous the difference between the timestamps
-                            # of two ringdowns must be less than this value.
+    delta_f_threshold = 0.5
+    delta_t_threshold = 3  # The severity is calculated for intervals of this size (in seconds)
+    delta_t_vicinity_limit = 20  # miliseconds. In order to be considered contiguous the difference between the
+                                 # timestamps of two ringdowns must be less than this value.
     dict_pmus = {18: 'UFPA', 27: 'UFMA', 36: 'UFJF', 45: 'UFAC', 54: 'UFAM', 63: 'UFBA', 72: 'UFRGS', 81: 'UNIFAP',
                  90: 'UNIFEI', 99: 'UNB', 108: 'COPPE', 117: 'UFC', 126: 'USP-SC', 135: 'UTFPR', 144: 'UFSC',
                  153: 'UNIR', 162: 'UFMT', 171: 'UNIPAMPA', 180: 'UFMG', 189: 'UFMS', 198: 'UFPE', 207: 'UFT'}
@@ -22,7 +23,7 @@ class DetectRingdown(MRJob):
                 if float(current_line['Value']) < 60 - self.delta_f_threshold:
                     yield current_line['HistorianID'], (current_line['Time'], current_line['Value'])
 
-    def reducer1(self, pmu, points):
+    def reducer(self, pmu, points):
         points_list = []
         number_of_points = 1
         severity = 0
@@ -32,10 +33,10 @@ class DetectRingdown(MRJob):
         for current_point in sorted(points_list, key=itemgetter(0)):
             current_timestamp = self.string_to_datetime(current_point[0])
             delta_t = abs(current_timestamp - previous_timestamp)
-            if delta_t.microseconds / 1000 <= self.delta_t_threshold:
+            if delta_t.microseconds / 1000 <= self.delta_t_vicinity_limit:
                 number_of_points += 1
             else:
-                severity = number_of_points / 300
+                severity = number_of_points / (self.delta_t_threshold * 60)
                 if severity > 0:
                     print(self.dict_pmus[pmu], [str(window_start_timestamp), severity])
                     severity = 0
@@ -44,8 +45,8 @@ class DetectRingdown(MRJob):
             previous_timestamp = current_timestamp
         # The following if treats the case in which the data chunk ends before reaching the "stop counting and print"
         # condition.
-        if delta_t.microseconds / 1000 <= self.delta_t_threshold:
-            severity = number_of_points / 300
+        if delta_t.microseconds / 1000 <= self.delta_t_vicinity_limit:
+            severity = number_of_points / (self.delta_t_threshold * 60)
             if severity > 0:
                 print(self.dict_pmus[pmu], [str(window_start_timestamp), severity])
 
